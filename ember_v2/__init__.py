9c394f886a6a2ffb09c1311c04adf36fade2b59d
Autumnmaple1 <hongyiming000003@163.com>
2026-03-15 21:23:32 +0800

    fix

--- ember_v2/__init__.py
+++
@@ -1,4 +0,0 @@
-# Ember V2 - Digital Life Engine
-# Refactored architecture with Packages and Actions
-
-__version__ = "2.0.0"
--- ember_v2/packages/db_process/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-DB Process Package
-PostgreSQL 数据库处理器
-"""
-
-from .client import DBProcess
-from .types import PoolStats, QueryResult, VectorSearchResult
-
-__all__ = ["DBProcess", "PoolStats", "QueryResult", "VectorSearchResult"]
